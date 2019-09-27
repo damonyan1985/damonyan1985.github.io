6eec0f3a8c53b2fad30c4237fa8c60117d57f77d
yanbo.boyia <2512854007@qq.com>
2019-09-27 09:33:04 +0800

    [fix]: upload new blog

--- precache-manifest.6d22812a259a0a65f6f319e7384078d4.js
+++ precache-manifest.6d22812a259a0a65f6f319e7384078d4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9060086cc0b625e99aa8d30cf9d33252",
+    "revision": "dc7893a97eff4447f55f04623fbf8e32",
     "url": "/index.html"
   },
   {
-    "revision": "0149840e2faa58c8c9d5",
-    "url": "/static/css/main.bd9aaa0d.chunk.css"
+    "revision": "89134715e2a05712148b",
+    "url": "/static/css/main.10a825a3.chunk.css"
   },
   {
     "revision": "151f406585fa8dcec1f6",
     "url": "/static/js/2.77d48aff.chunk.js"
   },
   {
-    "revision": "0149840e2faa58c8c9d5",
-    "url": "/static/js/main.9c00efe3.chunk.js"
+    "revision": "89134715e2a05712148b",
+    "url": "/static/js/main.1d00ff14.chunk.js"
   },
   {
     "revision": "bce8caf0d77330188749",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.489e4656b70015d2183451de3b7196b7.js"
+  "/precache-manifest.6d22812a259a0a65f6f319e7384078d4.js"
 );
 
 self.addEventListener('message', (event) => {
